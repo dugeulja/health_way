4aea8269b03fcca572ee4cf7af7421b8781b0103
codingDJ <jul897813@gmail.com>
2020-08-23 15:38:04 +0900

    makingAdapter
    1) firebase connection
    2) user file
    3) customAdapter

---
+++ app/src/main/java/com/example/healthway_dugeulja/CustomAdapter.java
@@ -0,0 +1,41 @@
+package com.example.healthway_dugeulja;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomviewHolder> {
+
+    @NonNull
+    @Override
+    public CustomviewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomviewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class CustomviewHolder extends RecyclerView.ViewHolder {
+        ImageView iv_profile;
+        TextView tv_id;
+        TextView tv_pw;
+        TextView tv_userName;
+        TextView tv_url;
+
+        public CustomviewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.iv_profile = itemView.findViewById(R.id.iv_profile);
+        }
+    }
+}
